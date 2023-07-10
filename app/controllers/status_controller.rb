4ec765976927cc5481db663c3540d8ddcf9bf2b5
Rudi Luzar <rudi@rudicode.com>
2023-07-10 11:18:51 -0400

    Fixed Bundler::RubyVersionMismatch error with passenger-status. Added memory_used stat.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -18,6 +18,7 @@ class StatusController < ApplicationController
     @system[:processors_available] = sys('nproc')
     memory_free = sys('free -m').split(" ")
     @system[:memory_total] = memory_free[7].to_i
+    @system[:memory_used] = memory_free[8].to_i
     @system[:memory_available] = memory_free[12].to_i
     ip_brief = sys('ip --brief address show').split("\n").each do |line|
       adapter = line.split(" ")[0]
@@ -31,10 +32,9 @@ class StatusController < ApplicationController
     @system[:lsb_release_codename] = sys('lsb_release -s -c')
     @system[:nginx_version] = sys('nginx -v 2>&1') # nginx outputs to stderr and we need to redirect it to stdout
     @system[:passenger_version] = sys('passenger -v')
-    # need sudo because passenger-status use passenger_system_ruby, which is differnt from the ruby in the Gemfile
-    # therfore the rails app does not run it.
-    # NOTE this only happens when running from inside the rails app
-    @system[:passenger_status] = sys('sudo passenger-status')
+    # NOTE: need to use the same ruby as the rails app to run an external ruby script
+    # from within the rails app. This avoids Bundler::RubyVersionMismatch error
+    @system[:passenger_status] = sys('ruby /usr/sbin/passenger-status')
     @system[:timedatectl] = sys('timedatectl')
     @system[:RVM_INFO] = sys('rvm info')
     @total_time = Time.current - start_time
